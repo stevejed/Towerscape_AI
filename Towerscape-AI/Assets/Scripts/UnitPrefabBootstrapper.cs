ae92248b18dafb2e3672e64c3045442b73c868c4
DESKTOP\jerds <jerds97@gmail.com>
2018-09-04 18:06:09 -0400

    Created Unit State class and began work on UnitPrefabBoostrapper

---
+++ Towerscape-AI/Assets/Scripts/UnitPrefabBootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPrefabBootstrapper : MonoBehaviour {
+
+    // Variables //
+
+    // Variables for: Unit Intelligence generation //
+    List<UnitIntelligence> unitPrefabs;
+    Object[] unitActionSets;
+
+    // Variables for: Unit State generation //
+    private UnitState[] unitStates;
+    public Object unitStateSet;
+
+
+    // Updates //
+
+	// Use this for initialization
+	void Start () {
+        // Generates Unit States
+        unitStates = new UnitState[unitPrefabs.Count];
+        for(int i = 0; i < unitPrefabs.Count; i++)
+        {
+            unitStates[i] = unitPrefabs[i].GetComponent<UnitState>();
+        }
+
+        //Generates Unit Intelligences
+        BehaviorSetReader reader = new BehaviorSetReader();
+        for(int i = 0; i < unitPrefabs.Count; i++)
+        {
+            reader.unitIntelligenceScript = unitPrefabs[i];
+            reader.actionSetFile = unitActionSets[i];
+            reader.ReadInBehaviors();
+        }
+	}
+}
---
+++ Towerscape-AI/Assets/Scripts/UnitState.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitState : MonoBehaviour {
+
+    // Variables //
+
+
+    // variables for: combat and movement //
+    float ammo;
+    float attackDamage;
+    float hp;
+    float range;
+    float speed;
+
+    // variables for: drops //
+    int goldDropped;
+    GameObject itemDropped;
+
+    // variables for: player interaction and spawning //
+    bool grabbable;
+    int unitCost;
+
+    // variables for: animation triggers //
+    /*TO-DO
+     * * Populate w/ animation triggers
+     */
+
+
+    // Methods //
+
+    // Method for: Taking damage and modifying the unit's health.  If it drops below 0, the unit dies.
+    public void takeDamage(float damage)
+    {
+        hp -= damage;
+        if(hp <= 0f)
+        {
+            this.die();
+        }
+    }
+
+    // Method for: killing the unit //
+    public void die()
+    {
+        Destroy(gameObject);
+    }
+
+    // Methods for: getting UnitState components //
+    public float getAmmo() { return ammo; }
+    public float getAttackDamage(){ return attackDamage; }
+    public float getHP() { return hp; }
+    public float getRange() { return range; }
+    public float getSpeed() { return speed; }
+    public bool getGrabbable() { return grabbable; }
+    public int getUnitCost() { return unitCost; }
+}
